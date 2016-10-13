087594b925ddfb277b59218555f1637ba06c783a
SanjithKanagavel <sanjithkanagavel@gmail.com>
2016-10-13 21:40:43 +0530

    Addition of button animations

--- Animation1/Animation 1/Animation 1/ViewController.m
+++ Animation1/Animation 1/Animation 1/ViewController.m
@@ -32,6 +32,8 @@
 {
     [super viewDidAppear:animated];
     [self animation1];
+    [self animation2];
+    [self animation3];
 }
 
 
@@ -40,7 +42,26 @@
     self.playBtn.transform = CGAffineTransformMakeScale(0.9, 0.9);
     [UIView animateWithDuration:1 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionRepeat animations:^{
         self.playBtn.transform = CGAffineTransformMakeScale(1.0,0.95);
+      } completion:nil ];
+}
+
+-(void)animation2{
+    
+    UIView *rectView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, self.playBtn1.frame.size.width, self.playBtn1.frame.size.height)];
+    rectView.layer.cornerRadius  = 20;
+    rectView.layer.borderColor = [UIColor greenColor].CGColor;
+    rectView.layer.borderWidth = 3.0f;
+    [self.playBtn1 addSubview:rectView];
+    
+    self.playBtn1.transform= CGAffineTransformMakeScale(1.0, 0.9);
+    [UIView animateWithDuration:1 delay:0.0 options:UIViewAnimationOptionCurveEaseInOut|UIViewAnimationOptionAutoreverse|UIViewAnimationOptionRepeat animations:^{
+        self.playBtn1.transform = CGAffineTransformMakeScale(1.2,1.2);
     } completion:nil ];
+    
+}
+
+-(void)animation3{
+    //UIButton *button = [UIButton alloc]initWithFrame:
 }
 
 - (void)didReceiveMemoryWarning {
