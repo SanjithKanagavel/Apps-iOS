775939e274b0b9070cf340f8ae698753ab6429fa
SanjithKanagavel <sanjithkanagavel@gmail.com>
2016-10-13 11:16:45 +0530

    Code push group chat

--- GroupChat/ToDoList/MasterViewController.m
+++ GroupChat/ToDoList/MasterViewController.m
@@ -75,36 +75,22 @@
     
     
     [self presentViewController:alertController animated:YES completion:nil];
-    
-    
-    
-    /*UIAlertView *dialog = [[UIAlertView alloc] initWithTitle:@"Enter a Todo :"
-     message:@"" delegate:self cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
-     dialog.tag = 1;
-     dialog.alertViewStyle = UIAlertViewStylePlainTextInput;
-     [dialog show];*/
-    
-    /*NSManagedObjectContext *context = [self.fetchedResultsController managedObjectContext];
-     NSEntityDescription *entity = [[self.fetchedResultsController fetchRequest] entity];
-     NSManagedObject *newManagedObject = [NSEntityDescription insertNewObjectForEntityForName:[entity name] inManagedObjectContext:context];
-     
-     // If appropriate, configure the new managed object.
-     // Normally you should use accessor methods, but using KVC here avoids the need to add a custom class to the template.
-     [newManagedObject setValue:[NSDate date] forKey:@"timeStamp"];
-     
-     // Save the context.
-     NSError *error = nil;
-     if (![context save:&error]) {
-     // Replace this implementation with code to handle the error appropriately.
-     // abort() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-     NSLog(@"Unresolved error %@, %@", error, [error userInfo]);
-     abort();
-     }*/
 }
 
 -(void)postMessagesToFirebase:(NSString *) str {
+   
+    NSDateFormatter *date  = [[NSDateFormatter alloc]init];
+    [date setDateFormat:@"HH:mm"];
+    NSDictionary *accountDetails = @{[date stringFromDate:[NSDate date]]: str};
+    NSDictionary *update1 = @{ @"/GroupChat/": accountDetails};
     
-    [[self.fdr child:@"GroupChats"] setValue:str forKey:@"chat"];
+    [self.fdr updateChildValues:update1 withCompletionBlock:^(NSError *error, FIRDatabaseReference *ref){
+        if (!error) {
+            NSLog(@"Completed");
+            /*NSDictionary *update2 = @{[@"/UserAccounts/" stringByAppendingString:(key)]: accountDetails};
+             [_ref updateChildValues:update2];*/
+        }
+    }];
 }
 
 #pragma mark - Segues
